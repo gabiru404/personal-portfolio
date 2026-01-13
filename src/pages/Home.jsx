9cb3434ee1f6ef37f7c49b91672e141efd4d3cb2
Gabriel Silva <gaabdev42@gmail.com>
2026-01-13 12:10:58 -0400

    refactor: criação pagina Home e organização de App

---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+    return (
+        <section>
+            <h1>Gabriel Sousa</h1>
+            <p>Desenvolvedor Backend</p>
+        </section>
+    );
+}
+
+export default Home;
