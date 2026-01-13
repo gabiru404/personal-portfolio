9b058cfdee1b4eb6bfbf0c38f94b94a97040b305
Gabriel Silva <gaabdev42@gmail.com>
2026-01-13 14:06:52 -0400

    added pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import Home from "./pages/Home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
 
 function App() {
     return (
-        <main>
-            <Home />
-        </main>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/sobre" element={<About />} />
+                <Route path="/projetos" element={<Projects />} />
+            </Routes>
+        </BrowserRouter>
     );
 }
 
---
+++ src/pages/Projects.jsx
@@ -0,0 +1,5 @@
+function Projects() {
+    return <h2>Meus projetos</h2>;
+}
+
+export default Projects;
